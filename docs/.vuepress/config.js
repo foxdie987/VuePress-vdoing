2323fb89618008409d75ed16ae36b6c15e1227b2
foxdie987 <kilnnumber1+git@gmail.com>
2021-11-16 10:01:46 +0700

    edit posts

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,8 @@ module.exports  =  {
   title: 'A Notebook',
   description: 'thử dùng VuePress',
   theme: 'vdoing',
+//  extend: 'vuepress-theme-vdoing',
+  //theme: require.resolve('/vuepress-theme-vdoing'),
   // theme: require.resolve('../../theme-vdoing'), // Use local theme package
   themeConfig: {
      sidebar: 'structuring' //  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义
